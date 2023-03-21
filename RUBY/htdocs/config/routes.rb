2dc369abb0baaa237b5d7700ad162c4aa06d87c1
youmahil <youmahil@bluedigm.com>
2023-03-21 21:57:23 +0900

    Update routes.rb

--- RUBY/htdocs/config/routes.rb
+++ RUBY/htdocs/config/routes.rb
@@ -2,7 +2,7 @@
 Rails.application.routes.draw do
   #root :to => 'welcome#index' , :as => 'home'
   #위 문장을 아래와 같이 변경한다(첫 페이지를 내 페이지로 설정)
-  root :to => 'my#page' , :vis => :get , :as => 'home'
+  root :to => 'my#page' , :via => :get , :as => 'home'
   #... 중간 생략 ...
 end
 #... 중간 생략 ...
