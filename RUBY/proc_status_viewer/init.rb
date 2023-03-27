8b4c9df383eb666179944e44fe4d51419620817e
youmahil <youmahil@bluedigm.com>
2023-03-27 10:54:21 +0900

    Create init.rb

---
+++ RUBY/proc_status_viewer/init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :proc_status_viewer do
+	name '공정관리 일감 조치자 추출 Plugin'
+	author '전길수'
+	description 'This is a BD Issue Process Status Viewer plugin for Redmine'
+	version '0.0.1'
+	url 'http"//www.bluedigm.com/plugins/proc_status_viewer'
+	author_url 'http"//www.bluedigm.com/about'
+end
