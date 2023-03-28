a725afed8f8facf07e9f08138189366fe4c343ef
youmahil <youmahil@bluedigm.com>
2023-03-28 14:47:11 +0900

    플러그인 라우팅

--- RUBY/proc_status_viewer/config/routes.rb
+++ RUBY/proc_status_viewer/config/routes.rb
@@ -2,3 +2,4 @@
 get 'proc_status_viewer', :to => 'proc_status_viewer#index' 
 get 'proc_status_viewer_view', :to => 'proc_status_viewer#view' 
 match 'proc_status_viewer/:type', :controller => 'proc_status_viewer', :action => 'index', :via => :get
+
